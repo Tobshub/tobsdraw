4096ab734b530fb792cfe979c71e767073b04c99
Tobshub <tobanigeorge@gmail.com>
2023-05-20 20:13:06 +0100

    var

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,8 @@ function DrawingCanvas({
       switch (ctxUtils.currentShape) {
         case "line": {
           if (x - startCoords.x === 0 && y - startCoords.y === 0) {
-            ctx.arc(x, y, Math.round(ctx.lineWidth / 2), 0, 360);
+            const r = Math.round(ctx.lineWidth / 2);
+            ctx.arc(x, y, r, 0, 360);
             ctx.fill();
           }
           break;
